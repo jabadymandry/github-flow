65c3fbb0b536bcf7b11c8a212a771f0a4a55c97b
Bruno Enée <brunoenee@gmail.com>
2022-01-17 15:50:36 +0300

    amelioration condition 2

--- 01-OOP/02-System-Parameters/calc.py
+++ 01-OOP/02-System-Parameters/calc.py
@@ -5,9 +5,7 @@ def main():
     # Importing in the main function will force Python to reload argv between each tests
     from sys import argv
     operator = ['+', '*', '-']
-    if argv[2] not in operator:
-        print(f"Les operateurs valide sont: {operator}")
-    if len(argv) > 3:
+    if len(argv) > 3 and argv[2] in operator:
         return eval(f'{argv[1]} {argv[2]} {argv[3]}')
     else:
         print("Merci de respecter le format: [operande-1] [operator] [operande-2]")
