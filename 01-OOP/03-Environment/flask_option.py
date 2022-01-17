a6489b4ff6888e9106528d8f635bd094d766f713
Bruno Enée <brunoenee@gmail.com>
2022-01-17 16:23:54 +0300

    test environnement OK

--- 01-OOP/03-Environment/flask_option.py
+++ 01-OOP/03-Environment/flask_option.py
@@ -3,7 +3,12 @@
 import os
 
 def start():
-    pass # TODO: return the right message
+    env = os.getenv('FLASK_ENV')
+    if env == "development":
+        data = "Starting in development mode..."
+    else:
+        data = "Starting in production mode..."
+    return data
 
 if __name__ == "__main__":
     print(start())
