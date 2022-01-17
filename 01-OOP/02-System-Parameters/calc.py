9546f55615af66b14e58c7047b8482d26aeb0737
Bruno Enée <brunoenee@gmail.com>
2022-01-17 15:09:08 +0300

    System parameters OK

--- 01-OOP/02-System-Parameters/calc.py
+++ 01-OOP/02-System-Parameters/calc.py
@@ -1,13 +1,21 @@
 # pylint: disable=missing-docstring
 
 
+import ssl
+
+
 def main():
     # We need to import argv inside the main() body to make our tests pass
     # Importing in the main function will force Python to reload argv between each tests
     from sys import argv
-
-    pass  # TODO: implement the calculator
-
+    operator = ['+', '*', '-']
+    if(len(argv) < 3):
+        print("Merci de respecter le format: [operande-1] [operator] [operande-2]")
+    elif(argv[2] not in operator):
+        print(f"Les operateurs valide sont: {operator}")
+    else:
+        return eval(f'{argv[1]} {argv[2]} {argv[3]}')
+        
 
 if __name__ == "__main__":
     print(main())
