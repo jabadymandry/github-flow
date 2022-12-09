e0c822e502bacd395466ee171f594c142594c7bd
MAINA BRUNO Enée <brunoenee@gmail.com>
2022-12-09 21:10:54 +0100

    implementation fonction convert

--- 01-OOP/06-Data-Structures/currencies.py
+++ 01-OOP/06-Data-Structures/currencies.py
@@ -2,8 +2,20 @@
 # pylint: disable=fixme
 # pylint: disable=unused-argument
 
-RATES = {} # TODO: add some currency rates
+RATES = { 
+         "USDEUR": 0.95,
+         "GBPEUR": 1.17,
+         "CHFEUR": 1.02
+        } # TODO: add some currency rates
 
 # `amount` is a `tuple` like (100, EUR). `currency` is a `string`
 def convert(amount, currency):
-    pass # TODO
+    #pass # TODO 
+    if  'USD' == amount[1]:
+        return round(amount[0] * RATES["USDEUR"])
+    elif amount[1] == "GBP":
+        return round(amount[0] * RATES["GBPEUR"])
+    elif amount[1] == "CHF":
+        return round(amount[0] * RATES["USDEUR"])
+    else:
+        return  None
