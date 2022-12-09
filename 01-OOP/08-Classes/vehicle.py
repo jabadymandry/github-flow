c5bc2aebaae89cbec477337998288da43635fc88
MAINA BRUNO Enée <brunoenee@gmail.com>
2022-12-10 00:10:50 +0100

    implementation de class Vehicle

--- 01-OOP/08-Classes/vehicle.py
+++ 01-OOP/08-Classes/vehicle.py
@@ -1,3 +1,18 @@
 # pylint: disable=missing-docstring
 
 # TODO: implement the `Vehicle` class
+class Vehicle:
+    """ """
+    def __init__(self, marque, couleur):
+        """ """
+        self.started = False
+        self.brand = marque
+        self.color = couleur
+        
+    def start(self):
+        """ """
+        self.started = True
+        
+    def stop(self):
+        """ """
+        self.started = False
